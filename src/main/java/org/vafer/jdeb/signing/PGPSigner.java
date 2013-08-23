c3c35090daa1c83fb3932893ada4d67b72926d7a
Max Garmash <max@garmash.org>
2013-08-23 21:41:47 +0600

    fix gpg verification

--- src/main/java/org/vafer/jdeb/signing/PGPSigner.java
+++ src/main/java/org/vafer/jdeb/signing/PGPSigner.java
@@ -16,26 +16,15 @@
 
 package org.vafer.jdeb.signing;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.security.GeneralSecurityException;
+import java.security.SignatureException;
 import java.util.Iterator;
 
 import org.bouncycastle.bcpg.ArmoredOutputStream;
 import org.bouncycastle.bcpg.BCPGOutputStream;
-import org.bouncycastle.openpgp.PGPException;
-import org.bouncycastle.openpgp.PGPPrivateKey;
-import org.bouncycastle.openpgp.PGPSecretKey;
-import org.bouncycastle.openpgp.PGPSecretKeyRing;
-import org.bouncycastle.openpgp.PGPSecretKeyRingCollection;
-import org.bouncycastle.openpgp.PGPSignature;
-import org.bouncycastle.openpgp.PGPSignatureGenerator;
-import org.bouncycastle.openpgp.PGPUtil;
+import org.bouncycastle.openpgp.*;
 import org.bouncycastle.openpgp.operator.bc.BcPBESecretKeyDecryptorBuilder;
 import org.bouncycastle.openpgp.operator.bc.BcPGPContentSignerBuilder;
 import org.bouncycastle.openpgp.operator.bc.BcPGPDigestCalculatorProvider;
@@ -75,35 +64,113 @@ public class PGPSigner {
      */
     public void clearSign(InputStream input, OutputStream output) throws IOException, PGPException, GeneralSecurityException {
         int digest = PGPUtil.SHA1;
-        
-        PGPSignatureGenerator signatureGenerator = new PGPSignatureGenerator(new BcPGPContentSignerBuilder(privateKey.getPublicKeyPacket().getAlgorithm(), digest));
-        signatureGenerator.init(PGPSignature.CANONICAL_TEXT_DOCUMENT, privateKey);
-        
-        ArmoredOutputStream armoredOutput = new ArmoredOutputStream(output);
-        armoredOutput.beginClearText(digest);
-        
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-        
-        String line;
-        while ((line = reader.readLine()) != null) {
-            // trailing spaces must be removed for signature calculation (see http://tools.ietf.org/html/rfc4880#section-7.1)
-            byte[] data = trim(line).getBytes("UTF-8");
-            
-            armoredOutput.write(data);
-            armoredOutput.write(EOL);
-            
-            signatureGenerator.update(data);
-            signatureGenerator.update(EOL);
+        PGPSignatureGenerator sGen = new PGPSignatureGenerator(new BcPGPContentSignerBuilder(privateKey.getPublicKeyPacket().getAlgorithm(), digest));
+        sGen.init(PGPSignature.CANONICAL_TEXT_DOCUMENT, privateKey);
+        input = new BufferedInputStream(input);
+        ArmoredOutputStream aOut = new ArmoredOutputStream(output);
+        aOut.beginClearText(digest);
+
+        //
+        // note the last \n/\r/\r\n in the file is ignored
+        //
+        ByteArrayOutputStream lineOut = new ByteArrayOutputStream();
+        int lookAhead = readInputLine(lineOut, input);
+
+        processLine(aOut, sGen, lineOut.toByteArray());
+
+        if (lookAhead != -1) {
+            do{
+                lookAhead = readInputLine(lineOut, lookAhead, input);
+
+                sGen.update((byte)'\r');
+                sGen.update((byte)'\n');
+
+                processLine(aOut, sGen, lineOut.toByteArray());
+            }
+            while (lookAhead != -1);
+        }
+        input.close();
+        aOut.endClearText();
+        sGen.generate().encode(new BCPGOutputStream(aOut));
+        aOut.close();
+
+    }
+
+    private static void processLine(OutputStream aOut, PGPSignatureGenerator sGen, byte[] line) throws SignatureException, IOException {
+        int length = getLengthWithoutWhiteSpace(line);
+        if (length > 0) {
+            sGen.update(line, 0, length);
+        }
+        aOut.write(line, 0, line.length);
+    }
+
+
+    private static int getLengthWithoutWhiteSpace(byte[] line) {
+        int    end = line.length - 1;
+        while (end >= 0 && isWhiteSpace(line[end])) {
+            end--;
+        }
+        return end + 1;
+    }
+
+    private static boolean isWhiteSpace(byte b) {
+        return b == '\r' || b == '\n' || b == '\t' || b == ' ';
+    }
+
+    private static int readInputLine(ByteArrayOutputStream bOut, InputStream fIn)
+            throws IOException
+    {
+        bOut.reset();
+
+        int lookAhead = -1;
+        int ch;
+
+        while ((ch = fIn.read()) >= 0)
+        {
+            bOut.write(ch);
+            if (ch == '\r' || ch == '\n')
+            {
+                lookAhead = readPassedEOL(bOut, ch, fIn);
+                break;
+            }
         }
 
-        armoredOutput.endClearText();
-        
-        PGPSignature signature = signatureGenerator.generate();
-        signature.encode(new BCPGOutputStream(armoredOutput));
-        
-        armoredOutput.close();
+        return lookAhead;
     }
-    
+
+    private static int readInputLine(ByteArrayOutputStream bOut, int lookAhead, InputStream fIn) throws IOException {
+        bOut.reset();
+
+        int ch = lookAhead;
+
+        do {
+            bOut.write(ch);
+            if (ch == '\r' || ch == '\n') {
+                lookAhead = readPassedEOL(bOut, ch, fIn);
+                break;
+            }
+        }
+        while ((ch = fIn.read()) >= 0);
+
+        if (ch < 0) {
+            lookAhead = -1;
+        }
+
+        return lookAhead;
+    }
+
+    private static int readPassedEOL(ByteArrayOutputStream bOut, int lastCh, InputStream fIn) throws IOException {
+        int lookAhead = fIn.read();
+
+        if (lastCh == '\r' && lookAhead == '\n') {
+            bOut.write(lookAhead);
+            lookAhead = fIn.read();
+        }
+
+        return lookAhead;
+    }
+
+
     /**
      * Returns the secret key matching the specified identifier.
      * 
