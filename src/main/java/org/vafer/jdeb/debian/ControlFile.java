2155f1b7f7a20a5240d94550d7c922e9e7493241
Lukas Roedl <lukas.roedl@ait.ac.at>
2013-10-30 11:29:54 +0100

    Fix issue with field overwritten with defaults when set empty in control file

--- src/main/java/org/vafer/jdeb/debian/ControlFile.java
+++ src/main/java/org/vafer/jdeb/debian/ControlFile.java
@@ -23,7 +23,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -38,8 +37,6 @@ import java.util.Set;
 public abstract class ControlFile {
 
     protected final Map<String, String> values = new LinkedHashMap<String, String>();
-    protected final Map<String, String> userDefinedFields = new LinkedHashMap<String, String>();
-    protected final Set<ControlField> userDefinedFieldNames = new HashSet<ControlField>();
 
     public void parse(String input) throws IOException, ParseException {
         parse(new ByteArrayInputStream(input.getBytes("UTF-8")));
@@ -54,10 +51,8 @@ public abstract class ControlFile {
             final String line = reader.readLine();
 
             if (line == null) {
-                if (buffer.length() > 0) {
-                    // flush value of the previous field
-                    set(field, buffer.toString());
-                }
+                // flush value of the previous field
+                set(field, buffer.toString());
                 break;
             }
 
@@ -72,11 +67,9 @@ public abstract class ControlFile {
 
                 // new field
 
-                if (buffer.length() > 0) {
-                    // flush value of the previous field
-                    set(field, buffer.toString());
-                    buffer = new StringBuilder();
-                }
+                // flush value of the previous field
+                set(field, buffer.toString());
+                buffer = new StringBuilder();
 
 
                 final int i = line.indexOf(':');
@@ -86,6 +79,7 @@ public abstract class ControlFile {
                 }
 
                 field = line.substring(0, i);
+                
                 buffer.append(line.substring(i + 1).trim());
 
                 continue;
@@ -101,23 +95,9 @@ public abstract class ControlFile {
 
     }
 
-    public void set(String field, final String value) {
-        if (!"".equals(value)) {
-
-            if (isUserDefinedField(field)) {
-                userDefinedFields.put(field, value);
-                String fieldName = getUserDefinedFieldName(field);
-
-                if (fieldName != null) {
-                    userDefinedFieldNames.add(new ControlField(fieldName));
-                }
-
-                field = fieldName;
-            }
-
-            if (field != null) {
-                values.put(field, value);
-            }
+    public void set(final String field, final String value) {
+        if (field != null && !"".equals(field)) {
+            values.put(field, value);
         }
     }
 
@@ -127,14 +107,6 @@ public abstract class ControlFile {
 
     protected abstract ControlField[] getFields();
 
-    protected Map<String, String> getUserDefinedFields() {
-        return userDefinedFields;
-    }
-
-    protected Set<ControlField> getUserDefinedFieldNames() {
-        return userDefinedFieldNames;
-    }
-
     public List<String> getMandatoryFields() {
         List<String> fields = new ArrayList<String>();
 
@@ -143,7 +115,7 @@ public abstract class ControlFile {
                 fields.add(field.getName());
             }
         }
-
+        
         return fields;
     }
 
@@ -153,7 +125,7 @@ public abstract class ControlFile {
 
     public Set<String> invalidFields() {
         Set<String> invalid = new HashSet<String>();
-
+        
         for (ControlField field : getFields()) {
             if (field.isMandatory() && get(field.getName()) == null) {
                 invalid.add(field.getName());
@@ -173,60 +145,6 @@ public abstract class ControlFile {
     }
 
     public String toString() {
-        List<ControlField> fields = new ArrayList<ControlField>();
-        fields.addAll(Arrays.asList(getFields()));
-        fields.addAll(getUserDefinedFieldNames());
-        return toString(fields.toArray(new ControlField[fields.size()]));
-    }
-
-    /**
-     * Returns the letter expected in the prefix of a user defined field
-     * in order to include the field in this control file.
-     *
-     * @return The letter returned is:
-     * <ul>
-     *   <li>B: for a binary package</li>
-     *   <li>S: for a source package</li>
-     *   <li>C: for a changes file</li>
-     * </ul>
-     * 
-     * @since 1.1
-     * @see <a href="http://www.debian.org/doc/debian-policy/ch-controlfields.html#s5.7">Debian Policy - User-defined fields</a>
-     */
-    protected abstract char getUserDefinedFieldLetter();
-
-    /**
-     * Tells if the specified field name is a user defined field.
-     * User-defined fields must begin with an 'X', followed by one or more
-     * letters that specify the output file and a hyphen.
-     * 
-     * @param field the name of the field
-     *
-     * @since 1.1
-     * @see <a href="http://www.debian.org/doc/debian-policy/ch-controlfields.html#s5.7">Debian Policy - User-defined fields</a>
-     */
-    protected boolean isUserDefinedField(String field) {
-        return field.startsWith("X") && field.indexOf("-") > 0;
-    }
-
-    /**
-     * Returns the user defined field without its prefix.
-     * 
-     * @param field the name of the user defined field
-     * @return the user defined field without the prefix, or null if the fields
-     *         doesn't apply to this control file.
-     * @since 1.1
-     */
-    protected String getUserDefinedFieldName(String field) {
-        int index = field.indexOf('-');
-        char letter = getUserDefinedFieldLetter();
-
-        for (int i = 0; i < index; ++i) {
-            if (field.charAt(i) == letter) {
-                return field.substring(index + 1);
-            }
-        }
-
-        return null;
+        return toString(getFields());
     }
 }
