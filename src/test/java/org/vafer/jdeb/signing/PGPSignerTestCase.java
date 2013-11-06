3e9e8fee2f54b3e5e4e07941a9499a34818c324b
Emmanuel Bourg <ebourg@apache.org>
2013-11-06 10:56:16 +0100

    Updated the dependency on BouncyCastle

--- src/test/java/org/vafer/jdeb/signing/PGPSignerTestCase.java
+++ src/test/java/org/vafer/jdeb/signing/PGPSignerTestCase.java
@@ -42,7 +42,7 @@ public final class PGPSignerTestCase extends TestCase {
                 "\n" +
                 "TEST3\n" +
                 "-----BEGIN PGP SIGNATURE-----\n" +
-                "Version: BCPG v1.48\n" +
+                "Version: BCPG v1.49\n" +
                 "\n" +
                 "iEYEARECABAFAkax1rgJEHM9pIAuB02PAABIJgCghFmoCJCZ0CGiqgVLGGPd/Yh5\n" +
                 "FQQAnRVqvI2ij45JQSHYJBblZ0Vv2meN\n" +
