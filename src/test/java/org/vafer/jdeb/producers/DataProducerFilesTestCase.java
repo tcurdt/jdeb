7ffa6fcb85c2093a9291f4e5bcbb717d48249dcc
tmortagne <thomas.mortagne@gmail.com>
2014-06-15 09:27:15 +0200

    Fix test on Windows

--- src/test/java/org/vafer/jdeb/producers/DataProducerFilesTestCase.java
+++ src/test/java/org/vafer/jdeb/producers/DataProducerFilesTestCase.java
@@ -1,18 +1,20 @@
 package org.vafer.jdeb.producers;
 
-import junit.framework.TestCase;
-import org.vafer.jdeb.DataConsumer;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import junit.framework.TestCase;
+
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.vafer.jdeb.DataConsumer;
 
 /**
  * Tests for {@link org.vafer.jdeb.producers.DataProducerFiles}.
@@ -73,7 +75,7 @@ public class DataProducerFilesTestCase extends TestCase {
         for (File f : Arrays.asList(file1, file2)) {
             verify(consumer).onEachFile(
                     any(FileInputStream.class),
-                    eq(f.getAbsolutePath()),
+                    eq(new TarArchiveEntry(f.getAbsolutePath(), true).getName()),
                     any(String.class),
                     eq("root"),
                     eq(0),
