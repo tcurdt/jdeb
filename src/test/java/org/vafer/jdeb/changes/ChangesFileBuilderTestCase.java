c7788ee5f8104d2e7e3e62682a62b6d692a5b856
Marko Koren <marko.koren@safesize.com>
2019-07-03 07:53:00 +0200

    updated auto-generated info

--- src/test/java/org/vafer/jdeb/changes/ChangesFileBuilderTestCase.java
+++ src/test/java/org/vafer/jdeb/changes/ChangesFileBuilderTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Marko.koren.
+ * Copyright 2019 The jdeb developers.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,10 +22,6 @@ import org.junit.Test;
 import org.vafer.jdeb.debian.BinaryPackageControlFile;
 import org.vafer.jdeb.debian.ChangesFile;
 
-/**
- *
- * @author Marko.koren
- */
 public class ChangesFileBuilderTestCase {
     
     @Test
