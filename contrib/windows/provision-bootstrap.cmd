6bfa390b8985d115378d9bffe1fae064e0ccde3d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-01 14:32:43 +0800

    Delete contrib directory

--- contrib/windows/provision-bootstrap.cmd
+++
@@ -1,1 +0,0 @@
-@powershell -NoProfile -ExecutionPolicy Bypass -File "c:\vagrant\install-chocolatey.ps1"
--- contrib/windows/provision-checkout.cmd
+++
@@ -1,6 +0,0 @@
-C:
-IF EXIST C:\Users\vagrant\Desktop\jdeb GOTO NOCHECKOUT
-  CD C:\Users\vagrant\Desktop
-  git clone https://github.com/tcurdt/jdeb.git
-:NOCHECKOUT
-
