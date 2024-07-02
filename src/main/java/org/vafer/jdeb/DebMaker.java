a88351718d816e6cf03dd4e18560eee022e7e006
Martin Chalupa <mchalupa@netflix.com>
2024-07-02 09:39:53 -0700

    Remove unused imports and replace * import with explicit classes

--- src/main/java/org/vafer/jdeb/DebMaker.java
+++ src/main/java/org/vafer/jdeb/DebMaker.java
@@ -22,7 +22,6 @@ import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.bouncycastle.crypto.digests.MD5Digest;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openpgp.PGPSignature;
