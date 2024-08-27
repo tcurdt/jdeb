0c6a2b4ed505b1013fa1f7dd454343341932b4f5
Jeremy Norris <jeremy.norris@lbnetworks.co>
2024-08-27 05:58:20 -0500

    Verify reproducible build timestamps

--- src/it/project-build-outputTimestamp/verify.groovy
+++ src/it/project-build-outputTimestamp/verify.groovy
@@ -1,1 +1,10 @@
-assert new File( basedir, 'target/jdeb-it_1.0_all.deb' ).exists();
+import org.apache.commons.compress.archivers.ar.ArArchiveEntry
+import org.apache.commons.compress.archivers.ar.ArArchiveInputStream
+
+File deb = new File( basedir, 'target/jdeb-it_1.0_all.deb' )
+assert deb.exists();
+ArArchiveInputStream is = new ArArchiveInputStream(new FileInputStream(deb))
+ArArchiveEntry entry
+while ((entry = is.getNextEntry()) != null) {
+    assert entry.getLastModifiedDate().getTime() == 1724680800000L
+}
